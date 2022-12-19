04680e28b1827527330203c6fe742f6413a8b341
Wasif Ansari <wasifansari2709@gmail.com>
2022-12-19 17:21:23 +0530

    creating a button to start test

--- internet speed test.py
+++ internet speed test.py
@@ -23,5 +23,9 @@ lab.place(x = 60 , y=290, height=50 ,width=380)
 lab_up = Label(sp,text = "00", font = ("Time New Roman",28,"bold","italic"))
 lab_up.place(x = 60 , y=360, height=50 ,width=380)
 
+button = Button(sp,text = "Run Test" , font = ("Time New Roman",28,"bold","italic") , relief=RAISED, bg = "red") 
+#RAISED TO LOOK IN 3D
+button.place(x = 60 , y=460, height=50 ,width=380)
+
 
 sp.mainloop() 
