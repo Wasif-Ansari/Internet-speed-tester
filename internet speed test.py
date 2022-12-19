ee729fec6a2b5ae3bd34f7fa8e863d94a7fb924a
Wasif Ansari <wasifansari2709@gmail.com>
2022-12-19 17:07:52 +0530

    making gui with tkinter and giving it size and name and background colour

--- internet speed test.py
+++ internet speed test.py
@@ -1,3 +1,12 @@
 
 from tkinter import *
 import speedtest
+
+
+sp = Tk() 
+sp.title(" INTERNET SPEED TEST ") #name
+sp.geometry("500x650") #size
+sp.config(bg = "blue") #background using config
+
+
+sp.mainloop() 
