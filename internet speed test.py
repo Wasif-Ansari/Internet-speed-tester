228bed1e1f0950dc8b13d10bcded0a33a618b37b
Wasif Ansari <wasifansari2709@gmail.com>
2022-12-19 17:09:58 +0530

    creating a heading label

--- internet speed test.py
+++ internet speed test.py
@@ -8,5 +8,9 @@ sp.title(" INTERNET SPEED TEST ") #name
 sp.geometry("500x650") #size
 sp.config(bg = "blue") #background using config
 
+lab = Label(sp,text = "Internet Speed Test", font = ("Time New Roman",30,"bold","italic"),bg = "blue",fg = "White")
+lab.place(x = 60 , y=40 , height=50 ,width=380)
+
+
 
 sp.mainloop() 
