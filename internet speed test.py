d5ed79ae95bb6fce645f44d5874d17e1388c752a
Wasif Ansari <wasifansari2709@gmail.com>
2022-12-19 17:12:35 +0530

    creating downloading speed label and its value

--- internet speed test.py
+++ internet speed test.py
@@ -11,6 +11,11 @@ sp.config(bg = "blue") #background using config
 lab = Label(sp,text = "Internet Speed Test", font = ("Time New Roman",30,"bold","italic"),bg = "blue",fg = "White")
 lab.place(x = 60 , y=40 , height=50 ,width=380)
 
+lab = Label(sp,text = "Downloading Speed", font = ("Time New Roman",28,"bold","italic"))
+lab.place(x = 60 , y=130, height=50 ,width=380)
+
+lab_down = Label(sp,text = "00", font = ("Time New Roman",28,"bold","italic"))
+lab_down.place(x = 60 , y=200, height=50 ,width=380)
 
 
 sp.mainloop() 
